1429e412ad697d374d6c05e924b7561c4553dcac
Michael Watzko <michael@watzko.de>
2018-02-16 22:31:27 +0100

    LED blinks

--- debug.sh
+++ debug.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 unset CARGO_INCREMENTAL
-#XARGO_RUST_SRC=/share/home/michael/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src \
-rustup run nightly xargo build \
- && arm-none-eabi-gdb target/thumbv7em-none-eabihf/debug/hebeanlage
+
+BIN="target/thumbv7em-none-eabihf/debug/hebeanlage"
+
+rustup run nightly xargo build  \
+ && arm-none-eabi-gdb "$BIN"
+# && sudo openocd \
+#        -f interface/stlink-v2.cfg \
+#        -f target/stm32f1x.cfg \
+#        -c init \
+#        -c targets \
+#        -c "halt" \
+#        -c "flash write_image erase $BIN" \
+#        -c "verify_image $BIN" \
+#        -c "reset run" \
+#        -c shutdown
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,11 @@ extern crate cortex_m_rt;
 extern crate cortex_m_semihosting;
 
 extern crate stm32f103xx;
+extern crate stm32f103xx_hal;
+
+
+
+use stm32f103xx_hal::prelude::*;
 
 use core::fmt::Write;
 
@@ -20,32 +25,77 @@ use self::cortex_m_semihosting::hio;
 
 
 fn main() {
-    let mut stdout = hio::hstdout().unwrap();
-    writeln!(stdout, "Hello, world from main!").unwrap();
+    stdout(|out| writeln!(out, "Hello World!"));
 
+    let mut cp = cortex_m::Peripherals::take().unwrap();
     let mut peripherals = stm32f103xx::Peripherals::take().unwrap();
 
-    // peripherals.GPIOC.odr.write(|w| unsafe {w.bits(0x44344444)});
-    peripherals.GPIOC.crh.write(|w| unsafe {w.bits(0x44344444)}); // set C13 to outpput
-    peripherals.GPIOC.odr.write(|w| w.odr13().set_bit());
+    //// peripherals.GPIOC.odr.write(|w| unsafe {w.bits(0x44344444)});
+    //peripherals.GPIOC.crh.write(|w| w.mode13().output()); // set C13 to outpput
+    //peripherals.GPIOC.odr.write(|w| w.odr13().set_bit());
 
+
+    let mut flash = peripherals.FLASH.constrain();
+    let mut rcc = peripherals.RCC.constrain();
+    let mut gpioc = peripherals.GPIOC.split(&mut rcc.apb2);
+
+    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let mut delay = stm32f103xx_hal::delay::Delay::new(cp.SYST, clocks);
+
+    loop {
+        led.set_high();
+        delay.delay_ms(1_000_u16);
+        led.set_low();
+        delay.delay_ms(1_000_u16);
+    }
+/*
 
     let mut state = false;
     loop {
         state = !state;
-        writeln!(stdout, "looped, state={}", state);
-        writeln!(stdout, "before=0x{:x}", peripherals.GPIOC.odr.read().bits());
+        if let Ok(ref mut stdout) = stdout {
+            writeln!(stdout, "looped, state={}", state);
+            writeln!(stdout, "before: {}", peripherals.GPIOC.idr.read().idr13().bit_is_set());
+        }
         if state {
-            peripherals.GPIOC.bsrr.write(|w| unsafe {w.bits(0xFF_FF_FF_FF)});
+            // peripherals.GPIOC.bsrr.write(|w| w.br13().reset());
+            peripherals.GPIOC.odr.write(|w| w.odr13().set_bit());
         } else {
-            peripherals.GPIOC.bsrr.write(|w| unsafe {w.bits(0x00_00_00_00)});
+            // peripherals.GPIOC.bsrr.write(|w| w.bs13().set());
+            peripherals.GPIOC.odr.write(|w| w.odr13().clear_bit());
         }
         for _ in 0..9999 {
 
         }
-        writeln!(stdout, "after =0x{:x}", peripherals.GPIOC.odr.read().bits());
-        writeln!(stdout, "");
+        if let Ok(ref mut stdout) = stdout {
+            writeln!(stdout, "after:  {}", peripherals.GPIOC.odr.read().odr13().bit_is_set());
+            writeln!(stdout, "");
+        }
+    }*/
+}
+
+#[cfg(debug_assertions)]
+fn stdout<A, F: FnOnce(&mut hio::HStdout) -> A>(f: F) {
+    static mut STDOUT : Option<hio::HStdout> = None;
+    unsafe {
+        if STDOUT.is_none() {
+            STDOUT = match hio::hstdout() {
+                Ok(stdout) => Some(stdout),
+                Err(_) => None
+            };
+        }
+        if let Some(ref mut stdout) = STDOUT {
+            f(stdout);
+        }
     }
+}
+
+#[cfg(not(debug_assertions))]
+fn stdout<A, F: FnOnce(&mut hio::HStdout) -> A>(f: F) {
+    // do nothing
 }
 
 // As we are not using interrupts, we just register a dummy catch all handler
@@ -54,11 +104,5 @@ fn main() {
 static INTERRUPTS: [extern "C" fn(); 240] = [default_handler; 240];
 
 extern "C" fn default_handler() {
-    asm::bkpt();
-}
-
-
-
-mod stm {
-
+    // asm::bkpt();
 }
