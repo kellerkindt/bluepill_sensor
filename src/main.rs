5b8dea5fcc965a9821464774b044e983a646879e
Michael Watzko <michael@watzko.de>
2018-03-09 19:02:37 +0100

    Finally able to read mac

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,13 @@ extern crate cortex_m_semihosting;
 
 extern crate stm32f103xx;
 extern crate stm32f103xx_hal;
+extern crate embedded_hal;
 
 extern crate byteorder;
 
 extern crate onewire;
 extern crate pcd8544;
+extern crate enc28j60;
 
 use stm32f103xx_hal::prelude::*;
 use stm32f103xx_hal::prelude::_embedded_hal_digital_OutputPin as OutputPin;
@@ -25,6 +27,12 @@ use stm32f103xx_hal::gpio::Output;
 use stm32f103xx_hal::gpio::OpenDrain;
 use stm32f103xx_hal::gpio::gpioc::PCx;
 use stm32f103xx_hal::delay::Delay;
+use stm32f103xx_hal::spi::Spi;
+
+use embedded_hal::spi::Mode;
+use embedded_hal::spi::Phase;
+use embedded_hal::spi::Polarity;
+use embedded_hal::blocking::spi;
 
 use stm32f103xx::GPIOC;
 
@@ -36,6 +44,7 @@ use self::cortex_m_semihosting::hio;
 
 use onewire::*;
 use pcd8544::*;
+use enc28j60::*;
 
 
 
@@ -63,12 +72,6 @@ fn stdout<A, F: FnOnce(&mut hio::HStdout) -> A>(_f: F) {
 }
 
 fn main() {
-    let mut test = 0_u8;
-    lolz(&mut test as *mut _);
-}
-
-fn lolz(probe0: *mut u8) {
-    let mut probe1 = 0u8;
 
     stdout(|out| writeln!(out, "Hello World!"));
 
@@ -127,8 +130,40 @@ fn lolz(probe0: *mut u8) {
     write!(display, "address: {}  {}  {}", probe0 as usize - 0x20000000_usize, (&mut "tes" as *mut _) as usize - 0x20000000_usize, (&mut probe2[0] as *mut _) as usize - 0x20000000_usize);
     */
     display.clear();
-    write!(display, "address: {}  {}", probe0 as usize - 0x20000000_usize, (&mut probe1 as *mut _) as usize - 0x20000000_usize);
 
+
+    let mut afio = peripherals.AFIO.constrain(&mut rcc.apb2);
+
+    // SPI1
+    let sck  = gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl);
+    let miso = gpioa.pa6.into_floating_input(&mut gpioa.crl);
+    let mosi = gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl);
+
+    let mut cs = gpioa.pa4.into_push_pull_output(&mut gpioa.crl);
+    let mut rs = gpioa.pa3.into_push_pull_output(&mut gpioa.crl);
+
+    rs.set_low();
+    delay.delay_ms(100_u16);
+    rs.set_high();
+
+
+    let mut spi = Spi::spi1(
+        peripherals.SPI1,
+        (sck, miso, mosi),
+        &mut afio.mapr,
+        Mode {
+            polarity: Polarity::IdleLow,
+            phase: Phase::CaptureOnFirstTransition,
+        },
+        1.mhz(), // upt to 8mhz?
+        clocks,
+        &mut rcc.apb2,
+    );
+
+
+    let mut enc = ENC28J60::new(spi, cs);
+
+    enc.init().unwrap();
     loop {
         led.set_low();
         delay.delay_ms(speed);
@@ -152,7 +187,8 @@ fn lolz(probe0: *mut u8) {
             }
         }
         display.set_light(true);
-        display.clear();
+        display.set_x_position(0);
+        display.set_y_position(0);
         writeln!(display, "TmpSensorAddr");
         let mut search = OneWireDeviceSearch::new();
         let addr = match wire.search_next(&mut search, &mut delay) {
@@ -170,27 +206,65 @@ fn lolz(probe0: *mut u8) {
             }
         };
         if let Some(ref device) = addr {
-            let ds18b20 = DS18B20::new(device.clone());
-            let _measure_resolution = ds18b20.measure_temperature(&mut wire, &mut delay);
-/*
-            match _measure_resolution {
-                Err(e) => {},
-                Ok(res)=> delay.delay_ms(res.time_ms())
-            };
-*/
-            let temp = ds18b20.read_temperature(&mut wire, &mut delay);
+            if let Ok(ref mut ds18b20) = DS18B20::new(device.clone()) {
+                let _measure_resolution = ds18b20.measure_temperature(&mut wire, &mut delay);
+                match _measure_resolution {
+                    Err(e) => {},
+                    Ok(res)=> delay.delay_ms(res.time_ms())
+                };
+                let temp = ds18b20.read_temperature(&mut wire, &mut delay);
 
-            writeln!(display);
-            match temp {
-                Ok(temp) => write!(display, " {:.1}°C", temp),
-                Err(err) => write!(display, "E: {:?}", e)
-            };
-            //write!(display, " {:.1}°C 0x{:02x}{:02x}", tempf32, content[0], content[1]);
-            //write!(display, " {:}  °C 0x{:02x}{:02x}", temp as i16 / 16_i16, content[0], content[1]);
-            /*for b in content.iter() {
-                write!(display, "{:02x} ", b);
-            }*/
-            delay.delay_ms(1_500_u16);
+                writeln!(display);
+                match temp {
+                    Ok(temp) => writeln!(display, " {:.1}°C", temp),
+                    Err(err) => writeln!(display, "E: {:?}", err)
+                };
+                //write!(display, " {:.1}°C 0x{:02x}{:02x}", tempf32, content[0], content[1]);
+                //write!(display, " {:}  °C 0x{:02x}{:02x}", temp as i16 / 16_i16, content[0], content[1]);
+                /*for b in content.iter() {
+                    write!(display, "{:02x} ", b);
+                }*/
+//            delay.delay_ms(1_500_u16);
+            } else {
+                writeln!(display, "family code mismatch");
+            }
+        }
+        if let Err(e) = enc.reset() {
+            writeln!(display, "1 {:?}", e);
+        } else {
+            if let Err(e) = enc.test() {
+                writeln!(display, "2 {:?}", e);
+            } else {
+                writeln!(display, "{:?}", enc.send(&[
+                    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // destination mac address
+                    0x22, 0x22, 0x22, 0x22, 0x22, 0x22, // source mac address,
+                    0x08, 0x00, // IPv4 Frame
+                    /// --- IP
+                    /*version*/ 0x40 | /*header length in 32bytes x */0x05,
+                    0x00, 0x00, // total IP Frame length TODO
+                    0b010_00000 /*dont fragment, last 5 bits fragment offset*/, 0x00, // fragment offset
+                    0xFF, // TTL
+                    17, // UDP
+                    0x00, 0x00, // checksum TODO
+                    192, 168, 3, 177, // source address
+                    192, 168, 3, 55, // destination address
+                    // --- UDP
+                ]));
+                /*
+                match enc.read_mac() {
+                    Err(e) => {
+                        writeln!(display, "3 {:?}", e);
+                    },
+                    Ok(addr) => {
+                        write!(display, "{:02x}", addr[5]);
+                        write!(display, ":{:02x}", addr[4]);
+                        write!(display, ":{:02x}", addr[3]);
+                        write!(display, ":{:02x}", addr[2]);
+                        write!(display, ":{:02x}", addr[1]);
+                        writeln!(display, ":{:02x}", addr[0]);
+                    }
+                };*/
+            }
         }
     }
 }
