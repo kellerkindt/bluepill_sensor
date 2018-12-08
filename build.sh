f1df1766e36c22150ff963586fae5970c3527480
Michael Watzko <michael@watzko.de>
2018-12-08 23:15:32 +0100

    Get flashing work again

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # rustup target add thumbv7m-none-eabi
-rustup run nightly xargo build --release --target=thumbv7m-none-eabi
+# rustup run nightly xargo build --release --target=thumbv7m-none-eabi
+cargo +nightly build --release
--- flash.sh
+++ flash.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 unset CARGO_INCREMENTAL
-BIN="target/thumbv7m-none-eabi/release/bluepill_sensor"
+BIN="../target/thumbv7m-none-eabi/release/bluepill_sensor"
 
 ./build.sh \
- && sudo openocd \
+ && openocd \
 	-f interface/stlink-v2.cfg \
 	-f target/stm32f1x.cfg \
 	-c init \
 	-c targets \
-	-c "halt" \
+        -c "halt" \
 	-c "flash write_image erase $BIN" \
 	-c "verify_image $BIN" \
 	-c "reset run" \
