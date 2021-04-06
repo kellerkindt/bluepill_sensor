83fcb99c36a5c8add73549c8ee875412e559f578
Michael Watzko <michael@watzko.de>
2021-04-06 19:11:06 +0200

    No longer default to a board-revision, require explicit feature flag

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![deny(broken_intra_doc_links)]
 
+#[cfg(not(feature = "board-rev-specified"))]
+compile_error!("Missing board revision selection (board-rev1, board-rev-2, board-rev-3, ...)");
+
 //#[macro_use(block_while)]
 #[macro_use(block)]
 extern crate nb;
